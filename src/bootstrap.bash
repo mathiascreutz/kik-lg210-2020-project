ae4d9883ac21263e4385c07cf8fac3ab309c33c0
mathiascreutz <mathias.jp.creutz@gmail.com>
2020-10-02 11:34:20 +0300

    Added gensim (tentatively)

--- src/bootstrap.bash
+++ src/bootstrap.bash
@@ -11,3 +11,6 @@ git clone https://github.com/mathiascreutz/kik-lg210-2020-project.git
 
 pip install nltk
 python -c "import nltk; nltk.download(['gutenberg', 'genesis', 'treebank', 'nps_chat', 'inaugural', 'webtext', 'wordnet', 'punkt', 'averaged_perceptron_tagger', 'brown', 'tagsets', 'cmudict', 'senseval', 'rte', 'reuters', 'names', 'stopwords', 'words', 'semcor', 'conll2000', 'conll2002', 'movie_reviews', 'sentence_polarity', 'twitter_samples', 'subjectivity', 'shakespeare', 'product_reviews_1', 'product_reviews_2'])"
+
+# Not part of the project work but an extension requested by a student
+pip install --upgrade gensim
